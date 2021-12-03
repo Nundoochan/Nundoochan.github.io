8c030059db322bfd068b24490dc53003091ea99e
Nundoochan <akshay.nundoochan@mail.mcgill.ca>
2021-12-03 00:13:10 -0500

    oki 4

--- script.js
+++ script.js
@@ -6,30 +6,30 @@ document.addEventListener("DOMContentLoaded", (e) => {
 	const cards = [
 		[
 			{
-				name: "pizza";
-				img: "images/pizza.png";
+				name: "pizza",
+				img: "images/pizza.png",
 			},
 			{
-				name: "milkshake";
-				img: "images/milkshake.png";
+				name: "milkshake",
+				img: "images/milkshake.png",
 			},
 			{
-				name: "hotdog";
-				img: "images/hotdog.png";
+				name: "hotdog",
+				img: "images/hotdog.png",
 			},
 		],
 		[
 			{
-				name: "cheeseburger";
-				img: "images/cheeseburger.png";
+				name: "cheeseburger",
+				img: "images/cheeseburger.png",
 			},
 			{
-				name: "ice-cream";
-				img: "images/ice-cream.png";
+				name: "ice-cream",
+				img: "images/ice-cream.png",
 			},
 			{
-				name: "fries";
-				img: "images/fries.png";
+				name: "fries",
+				img: "images/fries.png",
 			}
 		],
 	];
